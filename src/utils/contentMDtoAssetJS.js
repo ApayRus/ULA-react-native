d8222ba91a0f99957fb95d821783cbc372ea5d13
aparus <rustamapaev@gmail.com>
2020-11-04 03:33:15 +0300

    fix #14 smartypants - autoconvert --  to em dash

--- src/utils/contentMDtoAssetJS.js
+++ src/utils/contentMDtoAssetJS.js
@@ -1,96 +1,96 @@
-const marked = require("marked");
+const marked = require('marked')
 
+marked.use({ smartypants: true }) //"smart" typographic punctuation for things like quotes and dashes.
 
-const makeArrayFromMarkdown = (mdFileContent) =>
+const makeArrayFromMarkdown = mdFileContent =>
     marked
     .lexer(mdFileContent) // raw array with many unnecessary fields
-    .filter((elem) => elem.type !== "space")
-    .map((elem) => {
-        const { type, depth, text } = elem;
-        const joinedType = depth ? type[0] + depth : type[0];
-        return { type: joinedType, text };
-    });
+    .filter(elem => elem.type !== 'space')
+    .map(elem => {
+        const { type, depth, tokens = [] } = elem
+        const { text } = tokens[0] || {}
+        const joinedType = depth ? type[0] + depth : type[0]
+        return { type: joinedType, text }
+    })
 
-const prefixedIndex = (index) => {
-    return index.toString().padStart(3, "0");
-};
+const prefixedIndex = index => {
+    return index.toString().padStart(3, '0')
+}
 
 // object { key: param }
-const parseInfoParagraph = (text) => {
-    const rowsArray = text.split("\n");
+const parseInfoParagraph = text => {
+    const rowsArray = text.split('\n')
     const info = rowsArray.reduce((prev, item) => {
-        const [key, value] = item.split(":");
-        return {...prev, [key]: value.trim() };
-    }, {});
-    return info;
-};
+        const [key, value] = item.split(':')
+        return {...prev, [key]: value.trim() }
+    }, {})
+    return info
+}
 
 // object with number keys '001, 002, ...'
-const parseParagraph = (pText) => {
-    const rowsArray = pText.split("\n");
+const parseParagraph = pText => {
+    const rowsArray = pText.split('\n')
     const info = rowsArray.reduce((prev, item, index) => {
-        const rowIndex = prefixedIndex(index + 1);
-        return {...prev, [rowIndex]: { text: item.trim() } };
-    }, {});
-    return info;
-};
+        const rowIndex = prefixedIndex(index + 1)
+        return {...prev, [rowIndex]: { text: item.trim() } }
+    }, {})
+    return info
+}
 
-const parseInfoArray = (infoArray) => {
+const parseInfoArray = infoArray => {
     return infoArray.reduce((prev, item) => {
-        const { type, text } = item;
-        if (type === "h1") return { title: text };
-        if (type === "p") return {...prev, ...parseInfoParagraph(text) };
-    }, {});
-};
+        const { type, text } = item
+        if (type === 'h1') return { title: text }
+        if (type === 'p') return {...prev, ...parseInfoParagraph(text) }
+    }, {})
+}
 
-const parseChaptersArray = (markdownArray) => {
-    const chaptersArray = [];
-    let chapter = {};
-    let subchapterName = "";
+const parseChaptersArray = markdownArray => {
+    const chaptersArray = []
+    let chapter = {}
+    let subchapterName = ''
 
     markdownArray.forEach((elem, index, array) => {
-        const { type, text } = elem;
-        const { type: nextType } = array[index + 1] || {};
-        const isEndOfChapter = nextType === "h2" || !array[index + 1];
-        if (type === "h2") {
-            chapter = { title: text };
+        const { type, text } = elem
+        const { type: nextType } = array[index + 1] || {}
+        const isEndOfChapter = nextType === 'h2' || !array[index + 1]
+        if (type === 'h2') {
+            chapter = { title: text }
         }
-        if (type === "h3") {
-            subchapterName = text;
+        if (type === 'h3') {
+            subchapterName = text
         }
-        if (type === "p") {
+        if (type === 'p') {
             const subchapter = {
                 [subchapterName]: parseParagraph(text)
-            };
-            chapter = {...chapter, ...subchapter };
+            }
+            chapter = {...chapter, ...subchapter }
         }
         if (isEndOfChapter) {
-            chaptersArray.push(chapter);
+            chaptersArray.push(chapter)
         }
-    });
+    })
 
     const result = chaptersArray.reduce((prev, item, index) => {
-        const chapterIndex = prefixedIndex(index + 1);
-        return {...prev, [chapterIndex]: item };
-    }, {});
+        const chapterIndex = prefixedIndex(index + 1)
+        return {...prev, [chapterIndex]: item }
+    }, {})
 
-    return result;
-};
+    return result
+}
 
-const contentMDtoAssetJS = (mdFileContent) => {
+const contentMDtoAssetJS = mdFileContent => {
+    const markdownArray = makeArrayFromMarkdown(mdFileContent)
+    const elementTypesArray = markdownArray.map(elem => elem.type)
+    const infoEndIndex = elementTypesArray.indexOf('h2')
 
-    const markdownArray = makeArrayFromMarkdown(mdFileContent);
-    const elementTypesArray = markdownArray.map((elem) => elem.type);
-    const infoEndIndex = elementTypesArray.indexOf("h2");
+    const infoArray = markdownArray.slice(0, infoEndIndex)
+    const chaptersArray = markdownArray.slice(infoEndIndex)
 
-    const infoArray = markdownArray.slice(0, infoEndIndex);
-    const chaptersArray = markdownArray.slice(infoEndIndex);
-
-    const info = parseInfoArray(infoArray);
-    const chapters = parseChaptersArray(chaptersArray);
+    const info = parseInfoArray(infoArray)
+    const chapters = parseChaptersArray(chaptersArray)
 
     return { info, chapters }
-
 }
 
 module.exports = { contentMDtoAssetJS }
