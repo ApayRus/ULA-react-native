2af850d5edde90c764773dd3c5ddf310093db3fe
aparus <rustamapaev@gmail.com>
2020-12-20 09:39:05 +0300

    little styled

--- src/config/contentTypes.js
+++ src/config/contentTypes.js
@@ -1,30 +1,31 @@
 export default {
-    default: {
-        interactivity: 'oneLineOneFile'
-    },
-    words: {
-        style: {
-            container: {
-                alignItems: 'center'
-            },
-            item: { marginBottom: 20, alignItems: 'center' },
-            image: { width: 100, height: 100, resizeMode: 'contain' },
-            text: { alignSelf: 'center' },
-            translation: { marginTop: -10, alignSelf: 'center' }
-        }
-    },
+	default: {
+		interactivity: 'oneLineOneFile'
+	},
+	words: {
+		style: {
+			container: {
+				alignItems: 'center'
+			},
+			item: { marginBottom: 20, alignItems: 'center' },
+			image: { width: 100, height: 100, resizeMode: 'contain' },
+			text: { alignSelf: 'center' },
+			translation: { marginTop: -10, alignSelf: 'center' }
+		}
+	},
 
-    phrases: {
-        style: {
-            translation: {
-                marginTop: -10
-            }
-        }
-    },
-    text: {
-        interactivity: 'inText'
-    },
-    timing: {
-        interactivity: 'timing'
-    }
+	phrases: {
+		style: {
+			translation: {
+				marginTop: -10,
+				textAlign: 'right'
+			}
+		}
+	},
+	text: {
+		interactivity: 'inText'
+	},
+	timing: {
+		interactivity: 'timing'
+	}
 }
