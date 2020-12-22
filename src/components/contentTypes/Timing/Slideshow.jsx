3cfc43be7209f0f001cf140cbd082dddf3eb5a59
aparus <rustamapaev@gmail.com>
2020-12-22 05:30:35 +0300

    show/hide translations added

--- src/components/contentTypes/Timing/Slideshow.jsx
+++ src/components/contentTypes/Timing/Slideshow.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-import { useSelector } from 'react-redux'
 import { prefixedIndex } from '../../../utils/utils'
 
 export default function PhrasalPlayerView(props) {
@@ -10,11 +9,11 @@ export default function PhrasalPlayerView(props) {
 		contentType,
 		globalStyles,
 		chapterId,
-		showTranslation
+		showTranslation,
+		currentPhraseNum
 	} = props
 
-	const { currentPhraseNum } = useSelector(state => state.playerState)
-	const id = prefixedIndex(currentPhraseNum + 1)
+	const id = prefixedIndex(currentPhraseNum)
 	const phrasesCount = Object.keys(phrases).length
 	const { text, voice: { name: voiceName } = {} } = phrases[id]?.body[0] || {}
 	const { text: trText } = phrasesTr[id]?.body[0] || {}
@@ -25,9 +24,9 @@ export default function PhrasalPlayerView(props) {
 		<View /* style={contentTypeStyle.container} */>
 			<Text>{voiceName}</Text>
 			<Text style={globalStyles.body1}>{text}</Text>
-			<Text>{trText}</Text>
+			<Text style={{ opacity: showTranslation ? 1 : 0 }}>{trText}</Text>
 			<Text>
-				{currentPhraseNum + 1}/{phrasesCount}
+				{currentPhraseNum}/{phrasesCount}
 			</Text>
 		</View>
 	)
