3179d58084ca396868ebd7214509e392959eb34e
aparus <rustamapaev@gmail.com>
2020-12-28 05:42:15 +0300

    changed slider, from RNE to RN-community (in RNE there was bag with moving thumb in web)

--- src/components/contentTypes/Audio/index.jsx
+++ src/components/contentTypes/Audio/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { Text, View } from 'react-native'
-import { Slider, Icon } from 'react-native-elements'
+import Slider from '@react-native-community/slider'
+
 import { useSelector, useDispatch } from 'react-redux'
 import { setPlayerState } from '../../../store/playerStateActions'
 import PlayerControls from './PlayerControls'
@@ -36,36 +37,15 @@ export default function Audio(props) {
 				{chapterId}-{subchapterId}{' '}
 			</Text>
 			<Text>Audio!!! </Text>
+
 			<Slider
-				animateTransitions
-				animationType='timing'
-				maximumTrackTintColor='#ccc'
 				minimumValue={0}
 				value={currentTime}
-				maximumValue={duration}
-				minimumTrackTintColor='#222'
-				onSlidingComplete={() => console.log('onSlidingComplete()')}
-				onSlidingStart={() => console.log('onSlidingStart()')}
-				onValueChange={value => handleSeek(value)}
-				orientation='horizontal'
-				step={0.1}
-				style={{ width: '80%', height: 200, alignSelf: 'center' }}
-				thumbStyle={{ height: 20, width: 20 }}
-				thumbProps={{
-					children: (
-						<Icon
-							name='heartbeat'
-							type='font-awesome'
-							size={20}
-							reverse
-							containerStyle={{ bottom: 20, right: 20 }}
-							color='#f50'
-						/>
-					)
-				}}
-				thumbTintColor='#0c0'
-				thumbTouchSize={{ width: 40, height: 40 }}
-				trackStyle={{ height: 10, borderRadius: 20 }}
+				maximumValue={duration ? duration : 100}
+				onSlidingComplete={value => handleSeek(value)}
+				style={{ width: '80%', height: 40, alignSelf: 'center' }}
+				minimumTrackTintColor='blue'
+				maximumTrackTintColor='gray'
 			/>
 			<PlayerControls {...playerProps} />
 			<Text>
