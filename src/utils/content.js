c5cb5fbcc195d124499d0b4b0d64709c55b2a70f
aparus <rustamapaev@gmail.com>
2020-12-31 14:05:50 +0300

    parsing of subchapters moved to mdParser class

--- src/utils/content.js
+++ src/utils/content.js
@@ -3,14 +3,7 @@
 import original from '../../assets/content'
 import translations from '../../assets/translations'
 import { map, orderBy } from 'lodash'
-import { parseSubs as frazyParseSubs } from 'frazy-parser'
-import { prefixedIndex } from './utils'
-import marked from 'marked'
 import contentTypes from '../config/contentTypes'
-
-marked.use({
-    smartypants: true // additional typography like long tire -- , etc
-})
 
 export class Content {
     constructor(original, translations) {
@@ -82,33 +75,6 @@ export class Content {
         }
     }
 
-    /**
-     * get Subchapter with plain content,
-     * choose proper parser by type
-     * returns subchapter with parsed content
-     *
-     * @param {*} chapterId
-     * @param {*} subchapterId
-     * @param {*} subchapterDoc
-     */
-    parseSubchapter(chapterId, subchapterId, subchapterDoc) {
-        const { content } = subchapterDoc
-
-        const contentTypeDoc = this.getContentType(chapterId, subchapterId)
-
-        // switcher between parsers for each content type
-        const typeParserMap = {
-            oneLineOneFile: Content.parseTypeOneLineOneFile,
-            timing: Content.parseTypeTiming,
-            inText: Content.parseTypeInText
-        }
-
-        const { interactivity } = contentTypeDoc
-        const parserFunction = typeParserMap[interactivity]
-        const parsedContent = parserFunction ? parserFunction(content) : content
-        return {...subchapterDoc, content: parsedContent }
-    }
-
     getSubchapter(chapterId, subchapterId) {
         const subchapterDoc = this.original.content[chapterId].content[subchapterId]
         const parsedSubchapterDoc = this.parseSubchapter(
@@ -139,97 +105,13 @@ export class Content {
     }
 
     /**
-     *
-     * @param {string} subsText
-     * @returns {object} - phrases {start, end, text, voiceName}
-     */
-    static parseTypeTiming = subsText => {
-        // frazyParseSubs extracts text of phrase into tricky object {start, end, body:[{voice}]} for support multiple voices
-        // we don't need many voices (it's hard to handle this nested array of objects)
-        // then we extract it into plain object {start, end, text, voiceName}
-
-        const parsedSubs = frazyParseSubs(subsText)
-
-        if (!parsedSubs) return {}
-        const phrasesObject = {}
-
-        for (let key in parsedSubs) {
-            const { start, end, body = [] } = parsedSubs[key] || {}
-            const [bodyFirstObject] = body
-            const { voice: { name: voiceName } = {}, text = '' } =
-            bodyFirstObject || {}
-            phrasesObject[key] = { start, end, text, voiceName }
-        }
-
-        const emptyPhrase = { start: 0, end: 0, text: '' } // for add empty space before 1-st phrase
-        phrasesObject['000'] = emptyPhrase // empty phrase
-
-        return phrasesObject
-    }
-
-    /**
-     *
-     * @param {string} text
-     * @returns {object} - phrases {id:{text}}
-     */
-    static parseTypeOneLineOneFile = text => {
-        if (!text) return {}
-        const rowsArray = text.split('\n')
-        const obj = rowsArray.reduce((prev, item, index) => {
-            const rowIndex = prefixedIndex(index + 1)
-            const renderer = new marked.Renderer()
-            renderer.paragraph = text => text //by default renderer returns <p></p> for any text line
-            const text = marked(item.trim(), {
-                renderer
-            })
-            return {...prev, [rowIndex]: { text } }
-        }, {})
-        return obj
-    }
-
-    /**
-     *
-     * @param {string} textContent
-     * @returns {string} - html
-     */
-    static parseTypeInText = textContent => {
-        let html = marked(textContent)
-        html = Content.extractIntextShortcutIntoTags(html)
-        return html
-    }
-
-    /**
-     *
-     * @param {string} htmlText - multiline text
-     * @returns {string}
-     * @example
-     * const exampleText = 'There is some text with [[ sounded word ]] or [[sounded phrase | path to file]]'
-     * convertInTextShortcutIntoTags(exampleText)
-     * // 'There is some text with <inText text="sounded word" path="" /> or <inText text="sounded phrase" path="path to file" />'
-     * // () ===> <inText text="some text" path="path to file" />
-     */
-    static extractIntextShortcutIntoTags = htmlText => {
-        const inTextShortcutTemplate = new RegExp(
-                /\[\[\s*(.+?)\s*(\|\s*(.+?)\s*)?\]\]/gm
-            ) // [[ sounded word ]] or [[sounded phrase | path to file]]
-
-        return htmlText.replace(
-            inTextShortcutTemplate,
-            '<intext text="$1" path="$3"></intext>'
-        )
-    }
-
-    /**
      * content type contain 3 parts: name (key), interactivity, style
      * if something is not set, will be used default values
      * @param {string} type - name of type
      */
-    getContentType = (chapterId, subchapterId) => {
-        const subchapterDoc = this.original.content[chapterId].content[subchapterId]
-        const { type: typeRaw, title } = subchapterDoc
+    static getContentType = type => {
         const { default: defaultTypeObject } = contentTypes
-        const type = typeRaw ? typeRaw : title // if type not set, it is the same as title
-            // 1) type exist or not
+        // 1) type exist or not
         const typeObject = contentTypes[type] ?
             contentTypes[type] :
             defaultTypeObject
--- src/utils/contentMDtoAssetJS.js
+++ src/utils/contentMDtoAssetJS.js
@@ -1,5 +1,14 @@
 import yaml from 'yaml'
+import marked from 'marked'
+
 import { arrayToObject } from './utils.js'
+import { parseSubs as frazyParseSubs } from 'frazy-parser'
+import { prefixedIndex } from './utils'
+import { Content } from './content'
+
+marked.use({
+    smartypants: true // additional typography like long tire -- , etc
+})
 
 const h1template = new RegExp(/^\s*#{1}\s+(.+?)\s*(\[(.+?)\])?\s*$/, 'm')
 const h2template = new RegExp(/^\s*#{2}\s+(.+?)\s*(\[(.+?)\])?\s*$/, 'gm')
@@ -70,3 +79,110 @@ const parseMarkdown = (mdFileContent, h1template, h2template, h3template) => {
 
 export const contentMDtoAssetJS = text =>
     parseMarkdown(text, h1template, h2template, h3template)
+
+/**
+ * get Subchapter with plain content,
+ * choose proper parser by type
+ * returns subchapter with parsed content
+ *
+ * @param {*} chapterId
+ * @param {*} subchapterId
+ * @param {*} subchapterDoc
+ */
+const parseSubchapter = (chapterId, subchapterId, subchapterDoc) => {
+    const { content } = subchapterDoc
+
+    const contentTypeDoc = Content.getContentType(chapterId, subchapterId)
+
+    // switcher between parsers for each content type
+    const typeParserMap = {
+        oneLineOneFile: parseTypeOneLineOneFile,
+        timing: parseTypeTiming,
+        inText: parseTypeInText
+    }
+
+    const { interactivity } = contentTypeDoc
+    const parserFunction = typeParserMap[interactivity]
+    const parsedContent = parserFunction ? parserFunction(content) : content
+    return {...subchapterDoc, content: parsedContent }
+}
+
+/**
+ *
+ * @param {string} subsText
+ * @returns {object} - phrases {start, end, text, voiceName}
+ */
+const parseTypeTiming = subsText => {
+    // frazyParseSubs extracts text of phrase into tricky object {start, end, body:[{voice}]} for support multiple voices
+    // we don't need many voices (it's hard to handle this nested array of objects)
+    // then we extract it into plain object {start, end, text, voiceName}
+
+    const parsedSubs = frazyParseSubs(subsText)
+
+    if (!parsedSubs) return {}
+    const phrasesObject = {}
+
+    for (let key in parsedSubs) {
+        const { start, end, body = [] } = parsedSubs[key] || {}
+        const [bodyFirstObject] = body
+        const { voice: { name: voiceName } = {}, text = '' } = bodyFirstObject || {}
+        phrasesObject[key] = { start, end, text, voiceName }
+    }
+
+    const emptyPhrase = { start: 0, end: 0, text: '' } // for add empty space before 1-st phrase
+    phrasesObject['000'] = emptyPhrase // empty phrase
+
+    return phrasesObject
+}
+
+/**
+ *
+ * @param {string} text
+ * @returns {object} - phrases {id:{text}}
+ */
+const parseTypeOneLineOneFile = text => {
+    if (!text) return {}
+    const rowsArray = text.split('\n')
+    const obj = rowsArray.reduce((prev, item, index) => {
+        const rowIndex = prefixedIndex(index + 1)
+        const renderer = new marked.Renderer()
+        renderer.paragraph = text => text //by default renderer returns <p></p> for any text line
+        const text = marked(item.trim(), {
+            renderer
+        })
+        return {...prev, [rowIndex]: { text } }
+    }, {})
+    return obj
+}
+
+/**
+ *
+ * @param {string} textContent
+ * @returns {string} - html
+ */
+const parseTypeInText = textContent => {
+    let html = marked(textContent)
+    html = extractIntextShortcutIntoTags(html)
+    return html
+}
+
+/**
+ *
+ * @param {string} htmlText - multiline text
+ * @returns {string}
+ * @example
+ * const exampleText = 'There is some text with [[ sounded word ]] or [[sounded phrase | path to file]]'
+ * convertInTextShortcutIntoTags(exampleText)
+ * // 'There is some text with <inText text="sounded word" path="" /> or <inText text="sounded phrase" path="path to file" />'
+ * // () ===> <inText text="some text" path="path to file" />
+ */
+const extractIntextShortcutIntoTags = htmlText => {
+    const inTextShortcutTemplate = new RegExp(
+            /\[\[\s*(.+?)\s*(\|\s*(.+?)\s*)?\]\]/gm
+        ) // [[ sounded word ]] or [[sounded phrase | path to file]]
+
+    return htmlText.replace(
+        inTextShortcutTemplate,
+        '<intext text="$1" path="$3"></intext>'
+    )
+}
