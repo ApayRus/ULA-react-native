d97b020b028da7796a6a82597acd3710c9ef52cb
aparus <rustamapaev@gmail.com>
2021-01-06 06:49:44 +0300

    little fix (console removed)

--- src/utils/contentType.js
+++ src/utils/contentType.js
@@ -8,7 +8,7 @@ import contentTypes from '../config/contentTypes.js'
 import marked from 'marked'
 
 marked.use({
-	smartypants: true // additional typography like long tire -- , etc
+    smartypants: true // additional typography like long tire -- , etc
 })
 
 /**
@@ -21,23 +21,23 @@ marked.use({
  * @param {*} subchapterDoc
  */
 export const parseSubchapter = subchapterDoc => {
-	const { title, type: typeRaw, content } = subchapterDoc
+    const { title, type: typeRaw, content } = subchapterDoc
 
-	const type = typeRaw ? typeRaw : title
+    const type = typeRaw ? typeRaw : title
 
-	const contentTypeDoc = getContentType(type)
+    const contentTypeDoc = getContentType(type)
 
-	// switcher between parsers for each content type
-	const typeParserMap = {
-		oneLineOneFile: parseTypeOneLineOneFile,
-		timing: parseTypeTiming,
-		inText: parseTypeInText
-	}
+    // switcher between parsers for each content type
+    const typeParserMap = {
+        oneLineOneFile: parseTypeOneLineOneFile,
+        timing: parseTypeTiming,
+        inText: parseTypeInText
+    }
 
-	const { interactivity } = contentTypeDoc
-	const parserFunction = typeParserMap[interactivity]
-	const parsedContent = parserFunction ? parserFunction(content) : content
-	return { ...subchapterDoc, content: parsedContent }
+    const { interactivity } = contentTypeDoc
+    const parserFunction = typeParserMap[interactivity]
+    const parsedContent = parserFunction ? parserFunction(content) : content
+    return {...subchapterDoc, content: parsedContent }
 }
 
 /**
@@ -46,26 +46,26 @@ export const parseSubchapter = subchapterDoc => {
  * @returns {object} - phrases {start, end, text, voiceName}
  */
 const parseTypeTiming = subsText => {
-	// frazyParseSubs extracts text of phrase into tricky object {start, end, body:[{voice}]} for support multiple voices
-	// we don't need many voices (it's hard to handle this nested array of objects)
-	// then we extract it into plain object {start, end, text, voiceName}
+    // frazyParseSubs extracts text of phrase into tricky object {start, end, body:[{voice}]} for support multiple voices
+    // we don't need many voices (it's hard to handle this nested array of objects)
+    // then we extract it into plain object {start, end, text, voiceName}
 
-	const parsedSubs = frazyParseSubs(subsText)
+    const parsedSubs = frazyParseSubs(subsText)
 
-	if (!parsedSubs) return {}
-	const phrasesObject = {}
+    if (!parsedSubs) return {}
+    const phrasesObject = {}
 
-	for (let key in parsedSubs) {
-		const { start, end, body = [] } = parsedSubs[key] || {}
-		const [bodyFirstObject] = body
-		const { voice: { name: voiceName } = {}, text = '' } = bodyFirstObject || {}
-		phrasesObject[key] = { start, end, text, voiceName }
-	}
+    for (let key in parsedSubs) {
+        const { start, end, body = [] } = parsedSubs[key] || {}
+        const [bodyFirstObject] = body
+        const { voice: { name: voiceName } = {}, text = '' } = bodyFirstObject || {}
+        phrasesObject[key] = { start, end, text, voiceName }
+    }
 
-	const emptyPhrase = { start: 0, end: 0, text: '' } // for add empty space before 1-st phrase
-	phrasesObject['000'] = emptyPhrase // empty phrase
+    const emptyPhrase = { start: 0, end: 0, text: '' } // for add empty space before 1-st phrase
+    phrasesObject['000'] = emptyPhrase // empty phrase
 
-	return phrasesObject
+    return phrasesObject
 }
 
 /**
@@ -74,18 +74,18 @@ const parseTypeTiming = subsText => {
  * @returns {object} - phrases {id:{text}}
  */
 const parseTypeOneLineOneFile = text => {
-	if (!text) return {}
-	const rowsArray = text.split('\n')
-	const obj = rowsArray.reduce((prev, item, index) => {
-		const rowIndex = prefixedIndex(index + 1)
-		const renderer = new marked.Renderer()
-		renderer.paragraph = text => text //by default renderer returns <p></p> for any text line
-		const text = marked(item.trim(), {
-			renderer
-		})
-		return { ...prev, [rowIndex]: { text } }
-	}, {})
-	return obj
+    if (!text) return {}
+    const rowsArray = text.split('\n')
+    const obj = rowsArray.reduce((prev, item, index) => {
+        const rowIndex = prefixedIndex(index + 1)
+        const renderer = new marked.Renderer()
+        renderer.paragraph = text => text //by default renderer returns <p></p> for any text line
+        const text = marked(item.trim(), {
+            renderer
+        })
+        return {...prev, [rowIndex]: { text } }
+    }, {})
+    return obj
 }
 
 /**
@@ -94,9 +94,9 @@ const parseTypeOneLineOneFile = text => {
  * @returns {string} - html
  */
 export const parseTypeInText = textContent => {
-	let html = marked(textContent)
-	html = extractIntextShortcutIntoTags(html)
-	return html
+    let html = marked(textContent)
+    html = extractIntextShortcutIntoTags(html)
+    return html
 }
 
 /**
@@ -110,14 +110,14 @@ export const parseTypeInText = textContent => {
  * // () ===> <inText text="some text" path="path to file" />
  */
 const extractIntextShortcutIntoTags = htmlText => {
-	const inTextShortcutTemplate = new RegExp(
-		/\[\[\s*(.+?)\s*(\|\s*(.+?)\s*)?\]\]/gm
-	) // [[ sounded word ]] or [[sounded phrase | path to file]]
+    const inTextShortcutTemplate = new RegExp(
+            /\[\[\s*(.+?)\s*(\|\s*(.+?)\s*)?\]\]/gm
+        ) // [[ sounded word ]] or [[sounded phrase | path to file]]
 
-	return htmlText.replace(
-		inTextShortcutTemplate,
-		'<intext text="$1" path="$3"></intext>'
-	)
+    return htmlText.replace(
+        inTextShortcutTemplate,
+        '<intext text="$1" path="$3"></intext>'
+    )
 }
 
 /**
@@ -126,17 +126,14 @@ const extractIntextShortcutIntoTags = htmlText => {
  * @param {string} type - name of type
  */
 export const getContentType = type => {
-	const { default: defaultTypeObject } = contentTypes
-	// 1) type exist or not
-	const typeObject = contentTypes[type] ? contentTypes[type] : defaultTypeObject
-	// 2) type exist but some parts are not filled (interactivity or style)
-	// and we get them from
+    const { default: defaultTypeObject } = contentTypes
+    // 1) type exist or not
+    const typeObject = contentTypes[type] ? contentTypes[type] : defaultTypeObject
+        // 2) type exist but some parts are not filled (interactivity or style)
+        // and we get them from
 
-	console.log('type', type)
-	console.log('typeObject', typeObject)
-
-	let { interactivity, style } = typeObject
-	if (!interactivity) interactivity = defaultTypeObject.interactivity || {}
-	if (!style) style = defaultTypeObject.style || {}
-	return { type, interactivity, style }
+    let { interactivity, style } = typeObject
+    if (!interactivity) interactivity = defaultTypeObject.interactivity || {}
+    if (!style) style = defaultTypeObject.style || {}
+    return { type, interactivity, style }
 }
