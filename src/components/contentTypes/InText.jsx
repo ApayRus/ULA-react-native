056f5122b9c15c2e1cc4c4994a131fa4de800c28
aparus <rustamapaev@gmail.com>
2021-01-29 05:43:36 +0300

    add Quiz to inText element (first try)

--- src/components/contentTypes/InText.jsx
+++ src/components/contentTypes/InText.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, TouchableOpacity, useWindowDimensions } from 'react-native'
-import { Text } from 'react-native-elements'
+import { Text, CheckBox } from 'react-native-elements'
 import HTML from 'react-native-render-html'
 import { playAudio } from '../../utils/playerShortAudios'
 
@@ -16,13 +16,22 @@ function InText(props) {
 		trLang
 	} = props
 
-	const { title, content: html } = subchapterDoc
+	const {
+		title,
+		content: { html, quizKeys }
+	} = subchapterDoc
+
+	console.log('quizKeys', quizKeys)
 
 	const contentWidth = useWindowDimensions().width
 
 	const handlePress = (text, path) => () => {
 		const fileName = path || text
 		playAudio(fileName, 'intext')
+	}
+
+	const handlePressQuizVariant = (quizId, variantId) => () => {
+		console.log('quizId', chapterId, subchapterId, quizId, variantId)
 	}
 
 	const inTextRenderer = htmlAttribs => {
@@ -37,13 +46,38 @@ function InText(props) {
 		)
 	}
 
+	const quizRenderer = (htmlAttribs, children) => {
+		const { type, quizid: quizId } = htmlAttribs
+		return <View key={`quiz-${quizId}`}>{children}</View>
+	}
+
+	const variantRenderer = (htmlAttribs, children) => {
+		const { quizid: quizId, variantid: variantId, type } = htmlAttribs
+
+		return (
+			<TouchableOpacity
+				key={`variant-${quizId}-${variantId}`}
+				onPress={handlePressQuizVariant(quizId, variantId)}
+			>
+				<Text>
+					<CheckBox size={18} containerStyle={{ margin: 0, padding: 0 }} />{' '}
+					{children}
+				</Text>
+			</TouchableOpacity>
+		)
+	}
+
 	return (
 		<View style={{ padding: 5 }}>
 			<Text h2 h2Style={globalStyles.subchapter}>
 				{title}
 			</Text>
 			<HTML
-				renderers={{ intext: { renderer: inTextRenderer, wrapper: 'Text' } }}
+				renderers={{
+					intext: { renderer: inTextRenderer, wrapper: 'Text' },
+					quiz: { renderer: quizRenderer, wrapper: 'View' },
+					variant: { renderer: variantRenderer, wrapper: 'Text' }
+				}}
 				html={html}
 				contentWidth={contentWidth}
 			/>
