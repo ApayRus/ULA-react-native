95e8f41de8f00325fe698245494a13d112432e4a
aparus <rustamapaev@gmail.com>
2021-02-07 09:07:38 +0300

    big change to parser: app title and chapters now split with # (1 sign). for chapters it was ## before

--- src/utils/contentMDtoAssetJS.js
+++ src/utils/contentMDtoAssetJS.js
@@ -1,30 +1,22 @@
-import yaml from 'yaml'
+/**
+ * 1) first level parser, that split content by # and ## (h1, h2)
+ * into chapters and subchapters -- for navigation: cover + drawer
+ * 2) deepest parser, content type related, for each type of content
+ */
 
+import yaml from 'yaml'
 import { arrayToObject } from './utils.js'
 import { parseSubchapter } from './contentType.js'
 
-const h1template = new RegExp(/^\s*#{1}\s+(.+?)\s*(\[(.+?)\])?\s*$/, 'm')
+const h1template = new RegExp(/^\s*#{1}\s+(.+?)\s*(\[(.+?)\])?\s*$/, 'gm')
 const h2template = new RegExp(/^\s*#{2}\s+(.+?)\s*(\[(.+?)\])?\s*$/, 'gm')
-const h3template = new RegExp(/^\s*#{3}\s+(.+?)\s*(\[(.+?)\])?\s*$/, 'gm')
-
-// const h2s =
-const extractInfo = (markdownText, h1template, h2template) => {
-	const firstH2match = [...markdownText.matchAll(h2template)][0]
-	const { index: firstH2Index } = firstH2match
-	let infoText = markdownText.slice(0, firstH2Index)
-	const h1Match = infoText.match(h1template)
-	const [headerText, title = ''] = h1Match
-	infoText = infoText.replace(headerText, '').trim()
-	const info = yaml.parse(infoText)
-	return { title, ...info }
-}
 
 const splitMarkdownIntoPartsByTemplate = (text, template) => {
 	const matches = text.matchAll(template)
 	if (!text.match(template)) return [{ content: text }]
 	return [...matches].map((elem, index, array) => {
 		const nextElem = array[index + 1] || {}
-		const nextIndex = nextElem.index || 1000000000
+		const nextIndex = nextElem.index || text.length
 		const [headerText, title = '', , type = ''] = elem
 		const { index: curIndex, input } = elem
 		return {
@@ -35,41 +27,38 @@ const splitMarkdownIntoPartsByTemplate = (text, template) => {
 	})
 }
 
-const extractContent = (markdownText, h2template, h3template) => {
+const parseMarkdown = (markdownText, h1template, h2template) => {
 	const chaptersArray = splitMarkdownIntoPartsByTemplate(
 		markdownText,
-		h2template
+		h1template
 	)
 
-	let chaptersAndSubchapters = chaptersArray.map(elem => {
-		const subchapters = splitMarkdownIntoPartsByTemplate(
-			elem.content,
-			h3template
-		).map(subchapterDoc => parseSubchapter(subchapterDoc))
+	const infoBlock = chaptersArray.shift()
+	const { title, content: infoContent } = infoBlock || {}
+	const info = { title, ...yaml.parse(infoContent) }
 
-		return {
-			...elem,
-			content: subchapters
-		}
-	})
+	let chaptersAndSubchapters = chaptersArray
+		.map(elem => {
+			const subchapters = splitMarkdownIntoPartsByTemplate(
+				elem.content,
+				h2template
+			).map(subchapterDoc => parseSubchapter(subchapterDoc))
 
-	chaptersAndSubchapters = chaptersAndSubchapters.map(chapter => {
-		let { content } = chapter
-		content = arrayToObject(content)
-		return { ...chapter, content }
-	})
+			return {
+				...elem,
+				content: subchapters
+			}
+		})
+		.map(chapter => {
+			let { content } = chapter
+			content = arrayToObject(content)
+			return { ...chapter, content }
+		})
 
-	chaptersAndSubchapters = arrayToObject(chaptersAndSubchapters)
+	const content = arrayToObject(chaptersAndSubchapters)
 
-	return chaptersAndSubchapters
-}
-
-// console.log("chaptersAndSubchaptersArray", JSON.stringify(chaptersAndSubchapters, null, '\t'));
-const parseMarkdown = (mdFileContent, h1template, h2template, h3template) => {
-	const info = extractInfo(mdFileContent, h1template, h2template)
-	const content = extractContent(mdFileContent, h2template, h3template)
 	return { info, content }
 }
 
 export const contentMDtoAssetJS = text =>
-	parseMarkdown(text, h1template, h2template, h3template)
+	parseMarkdown(text, h1template, h2template)
