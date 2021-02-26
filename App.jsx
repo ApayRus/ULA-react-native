9aa855dc5a3bfe6b363301e78e29733832f3de35
aparus <rustamapaev@gmail.com>
2021-02-26 18:45:30 +0300

    change after reinstall expo init

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import AppLoading from 'expo-app-loading'
+import { useFonts } from 'expo-font'
+import { Provider as ReduxProvider } from 'react-redux'
+import { ThemeProvider } from 'react-native-elements'
+import store from './src/store/rootReducer'
+import RootNavigation from './src/navigation/RootNavigation'
+import content from './src/utils/content'
+
+export default function App() {
+	const fonts = content.getFonts()
+	const [fontLoaded] = useFonts(fonts)
+
+	return fontLoaded ? (
+		<ReduxProvider store={store}>
+			<ThemeProvider>
+				<RootNavigation />
+			</ThemeProvider>
+		</ReduxProvider>
+	) : (
+		<AppLoading />
+	)
+}
