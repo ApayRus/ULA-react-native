9d785caf3b84f8b4b1c8c183151d700ea501a632
aparus <rustamapaev@gmail.com>
2021-04-12 00:22:52 +0300

    rewrite task text without switch

--- src/components/contentTypes/Exercise/Single2.jsx
+++ src/components/contentTypes/Exercise/Single2.jsx
@@ -29,57 +29,42 @@ const Single2 = () => {
 		givenType: 'audio',
 		givenLang: 'original',
 		requiredType: 'text',
-		requiredLang: 'original',
-		activityType: 'choose-from-4',
+		requiredLang: 'translation',
+		activityType: 'write',
 		count: '10'
 	}
 
 	const getTask1 = (givenType, givenLang = '') => {
-		switch (`${givenType} | ${givenLang}`) {
-			case 'audio | ':
-				return 'Listen to the audio'
-			case 'text | original':
-				return 'Read the original text'
-			case 'text | translation':
-				return 'Read the translation text'
-			default:
-				return null
+		const mapOfTaskTexts = {
+			'audio-original': 'Listen to the audio',
+			'text-original': 'Read the original text',
+			'text-translation': 'Read the translation text'
 		}
+		const task = `${givenType}-${givenLang}`
+		return mapOfTaskTexts?.[task]
 	}
 
 	const getTask2 = (givenType, requiredType, requiredLang, activityType) => {
 		const activity = activityType === 'write' ? 'write' : 'choose'
-		switch (
-			`${givenType}-${givenLang} --> ${requiredType}-${requiredLang} ${activity}`
-		) {
-			case 'audio-original --> text-original write':
-				return 'write what you have heard'
-			case 'audio-original --> text-translation write':
-				return 'write translation'
-			case 'audio-original --> text-original choose':
-				return 'choose the right variant'
-			case 'audio-original --> text-translation choose':
-				return 'choose the right translation'
-
-			case 'text-original --> text-translation write':
-				return 'write translation'
-			case 'text-translation --> text-original write':
-				return 'write original text'
-
-			default:
-				return null
+		const mapOfTaskTexts = {
+			'audio-original --> text-original write': 'write what you have heard',
+			'audio-original --> text-translation write': 'write translation',
+			'audio-original --> text-original choose': 'choose the right variant',
+			'audio-original --> text-translation choose':
+				'choose the right translation',
+			'text-original --> text-translation write': 'write translation',
+			'text-translation --> text-original write': 'write original text'
 		}
+		const task = `${givenType}-${givenLang} --> ${requiredType}-${requiredLang} ${activity}`
+		return mapOfTaskTexts?.[task]
 	}
 
 	const getPlaceholderText = requiredLang => {
-		switch (requiredLang) {
-			case 'original':
-				return 'original text'
-			case 'translation':
-				return 'translation text'
-			default:
-				return null
+		const mapOfTexts = {
+			original: 'original text',
+			translation: 'translation text'
 		}
+		return mapOfTexts?.[requiredLang]
 	}
 
 	const {
@@ -90,7 +75,7 @@ const Single2 = () => {
 		requiredType
 	} = exerciseInfo
 
-	const task1 = getTask1(givenType)
+	const task1 = getTask1(givenType, givenLang)
 	const task2 = getTask2(givenType, requiredType, requiredLang, activityType)
 	const taskText = `${task1}, and ${task2}`
 	const placeholderText = getPlaceholderText(requiredLang)
