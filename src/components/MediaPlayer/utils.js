550b1323fec3f5ad3bba0182220e10f10cb65e77
aparus <rustamapaev@gmail.com>
2021-06-15 10:26:51 +0300

    moved functions for youtube to mediaPlayer

--- src/components/MediaPlayer/utils.js
+++ src/components/MediaPlayer/utils.js
@@ -1,5 +1,4 @@
 import content from '../../utils/content'
-import { fetchYoutubeVideoByUrl, isYoutube } from '../../utils/utils'
 import PlayerBasic from './playerBasicClass'
 import PlayerPhrasal from './playerPhrasalClass'
 import { Audio } from 'expo-av'
@@ -84,4 +83,53 @@ export const formatSecondsToTime = inputSeconds => {
 	const seconds = totalSeconds % 60
 	const secondsString = seconds.toString().padStart(2, '0')
 	return `${hoursString}${minutes}:${secondsString}`
+}
+
+/**
+ * extracts videoId from youtube url
+ * @param {string} url
+ */
+const getYoutubeId = url => {
+	var id = ''
+	url = url
+		.replace(/(>|<)/gi, '')
+		.split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/)
+	if (url[2] !== undefined) {
+		id = url[2].split(/[^0-9a-z_\-]/i)
+		id = id[0]
+	} else {
+		id = url
+	}
+	return id
+}
+
+/**
+ * Checks is url from youtube or not
+ * @param {string} url
+ */
+const isYoutube = (url = '') => {
+	return Boolean(
+		url.match(/^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+/)
+	)
+}
+
+const fetchYoutubeVideoByUrl = async url => {
+	const youtubeId = getYoutubeId(url)
+	let response = null
+	try {
+		response = await fetch(
+			`https://direct-link.vercel.app/api/video/${youtubeId}`,
+			//http://192.168.0.189:3000
+			{
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json'
+				}
+			}
+		)
+		response = await response.json()
+	} catch (err) {
+		console.log('TRY/CATCH ERROR', err)
+	}
+	return response
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -11,55 +11,6 @@ export const prefixedIndex = (index = '') => {
 	return index.toString().padStart(3, '0')
 }
 
-/**
- * extracts videoId from youtube url
- * @param {string} url
- */
-const getYoutubeId = url => {
-	var id = ''
-	url = url
-		.replace(/(>|<)/gi, '')
-		.split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/)
-	if (url[2] !== undefined) {
-		id = url[2].split(/[^0-9a-z_\-]/i)
-		id = id[0]
-	} else {
-		id = url
-	}
-	return id
-}
-
-/**
- * Checks is url from youtube or not
- * @param {string} url
- */
-export const isYoutube = (url = '') => {
-	return Boolean(
-		url.match(/^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+/)
-	)
-}
-
-export const fetchYoutubeVideoByUrl = async url => {
-	const youtubeId = getYoutubeId(url)
-	let response = null
-	try {
-		response = await fetch(
-			`https://direct-link.vercel.app/api/video/${youtubeId}`,
-			//http://192.168.0.189:3000
-			{
-				headers: {
-					Accept: 'application/json',
-					'Content-Type': 'application/json'
-				}
-			}
-		)
-		response = await response.json()
-	} catch (err) {
-		console.log('TRY/CATCH ERROR', err)
-	}
-	return response
-}
-
 export const splitMarkdownIntoPartsByTemplate = (text, template) => {
 	const matches = [...text.matchAll(template)]
 	if (!matches) return [{ content: text }]
